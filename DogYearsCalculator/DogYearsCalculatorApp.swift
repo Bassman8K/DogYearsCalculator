f51abc337ace6bd7450265981f982718183de975
Jonathan Edwin <113654202+Bassman8K@users.noreply.github.com>
2025-02-20 10:56:01 +1100

    Initial Commit

---
+++ DogYearsCalculator/DogYearsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogYearsCalculatorApp.swift
+//  DogYearsCalculator
+//
+//  Created by Jonathan Edwin on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogYearsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
