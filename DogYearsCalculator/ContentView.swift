78a3e5569f5901d2aa3ef64552e9a1f419ebd574
Jonathan Edwin <113654202+Bassman8K@users.noreply.github.com>
2025-02-20 11:31:55 +1100

    I have added the begnning code for the commit

--- DogYearsCalculator/ContentView.swift
+++ DogYearsCalculator/ContentView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var age = 0.0
+    
+    func calculateDogYears(double conversionFactor: Double) -> Double {
+        return age * conversionFactor
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            switch Int(calculateDogYears(double: 7)) {
+                case 0...7:
+                    Text("🐶")
+                        .font(.system(size: 70))
+                case 8...15:
+                    Text("🐕")
+                        .font(.system(size: 70))
+                default:
+                    Text("😇")
+                        .font(.system(size: 70))
+            }
+            
+            Text("Dog Years Calculator")
+                .font(.largeTitle)
+                .fontDesign(.rounded)
+                .fontWeight(.black)
+            
+            Slider(value: $age, in: 0...100)
+            Text("My age: \(Int(age))")
+            Text("Dog's age: \(Int(calculateDogYears(double: 7)))")
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
